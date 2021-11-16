38e8720412dfd9031fb4410e716df4886fc7646b
Benjamin Rose <benthere914@gmail.com>
2021-11-16 15:22:12 -0600

    structure and style for signup form

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { Redirect } from 'react-router-dom';
 import { signUp } from '../../store/session';
+import './signUpForm.css'
 
 const SignUpForm = () => {
     const [errors, setErrors] = useState([]);
@@ -12,6 +13,7 @@ const SignUpForm = () => {
     const [viewPassword, setViewPassword] = useState(false)
     const [repeatPassword, setRepeatPassword] = useState('');
     const [viewRepeatPassword, setViewRepeatPassword] = useState(false)
+    const [validImg, setValidImg] = useState(true)
     const user = useSelector(state => state.session.user);
     const dispatch = useDispatch();
 
@@ -23,36 +25,83 @@ const SignUpForm = () => {
         }
     };
 
-
+    const imgErrorHandler = (e) => {
+        e.target.onerror = null;
+        e.target.src="https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png"
+        setValidImg(false)
+    }
     if (user) {
         return <Redirect to='/' />;
     }
 
     return (
-        <form onSubmit={onSignUp}>
+        <form onSubmit={onSignUp} className='signUpForm'>
+            <h2>Sign up to continue</h2>
+            <p>To purchase an item, or to chat with a seller, please create your account</p>
+            <img
+                src={icon?icon:'https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png'}
+                onError={(e) => {imgErrorHandler(e)}}
+                onLoad={()=> {setValidImg(true)}}
+            />
             <div>{errors.map((error, ind) => (<div key={ind}>{error}</div>))}</div>
-            <div>
+            <div className='formData'>
                 <label>Email</label>
-                <input type='text' name='email' onChange={(e) => {setEmail(e.target.value)}} value={email}></input>
+                <input
+                    type='text'
+                    name='email'
+                    onChange={(e) => {setEmail(e.target.value)}}
+                    value={email}
+                    placeholder='Enter your email'>
+                </input>
             </div>
-            <div>
+            <div className='formData'>
                 <label>User Name</label>
-                <input type='text' name='username' onChange={(e) => {setUsername(e.target.value)}} value={username}></input>
+                <input
+                    type='text'
+                    name='username'
+                    onChange={(e) => {setUsername(e.target.value)}}
+                    value={username}
+                    placeholder='Create your username'>
+                </input>
             </div>
-            <div>
-                <label>Icon</label>
-                <input type='text' name='icon' onChange={(e) => {setIcon(e.target.value)}} value={icon}required={true}></input>
+            <div className='formData'>
+                <label>Icon - optional</label>
+                <input
+                    type='text'
+                    name='icon'
+                    onChange={(e) => {setIcon(e.target.value)}}
+                    value={icon}required={true}
+                    placeholder='Enter your photo url'>
+                </input>
+                <p>Image changes upon valid url</p>
             </div>
-            <div>
+            <div className='formData'>
                 <label>Password</label>
-                <input type={viewPassword?'text':'password'} name='password' onChange={(e) => {setPassword(e.target.value)}} value={password}></input>
-                <i className={viewPassword?'fas fa-eye':'fas fa-eye-slash'} onClick={() => {setViewPassword((current) => !current)}}></i>
+                <input
+                    type={viewPassword?'text':'password'}
+                    name='password' onChange={(e) => {setPassword(e.target.value)}}
+                    value={password}
+                    placeholder='Create a password'>
+                </input>
+                <i
+                    className={viewPassword?'fas fa-eye':'fas fa-eye-slash'}
+                    onClick={() => {setViewPassword((current) => !current)}}>
+                </i>
             </div>
-            <div>
+            <div className='formData'>
                 <label>Confirm Password</label>
-                <input type={viewRepeatPassword?'text':'password'} name='repeat_password' onChange={(e) => {setRepeatPassword(e.target.value)}} value={repeatPassword} required={true}></input>
-                <i className={viewRepeatPassword?'fas fa-eye':'fas fa-eye-slash'} onClick={() => {setViewRepeatPassword((current) => !current)}}></i>
-
+                <input
+                    type={viewRepeatPassword?'text':'password'}
+                    name='repeat_password'
+                    onChange={(e) => {setRepeatPassword(e.target.value)}}
+                    value={repeatPassword}
+                    required={true}
+                    placeholder='Confirm your password'>
+                </input>
+                <i
+                    className={viewRepeatPassword?'fas fa-eye':'fas fa-eye-slash'}
+                    onClick={() => {setViewRepeatPassword((current) => !current)}}>
+                </i>
             </div>
             <button type='submit'>Sign Up</button>
         </form>
