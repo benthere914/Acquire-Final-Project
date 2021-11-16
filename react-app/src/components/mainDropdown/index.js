2eb73730087f58b8b4487decbcad3b600e8bf231
Benjamin Rose <benthere914@gmail.com>
2021-11-16 17:46:01 -0600

    switches dropdown depending if you are logged in

--- react-app/src/components/mainDropdown/index.js
+++ react-app/src/components/mainDropdown/index.js
@@ -3,10 +3,17 @@
 // import { NavLink } from 'react-router-dom';
 // import LogoutButton from './auth/LogoutButton';
 import './index.css'
+import { logout } from '../../store/session';
+import { useDispatch, useSelector } from 'react-redux';
+
 
 const MainDropdown = () => {
+    const dispatch = useDispatch()
+    const user = useSelector(state => state.session.user)
+    console.log(user)
     return (
     <>
+    {user?.username?
     <div className='mainDropDown'>
         <div className='topOfMainDropDown'>
             <img className='userPhoto' src='https://secure.gravatar.com/avatar/5c88f00c5c666cf560ecf0a704b84e35?secure=true&size=300' alt='username'/>
@@ -28,12 +35,29 @@ const MainDropdown = () => {
                 <i className='fas fa-comment-dots'/>
                 <p>messages</p>
             </li>
-            <li>
+            <li onClick={() => {dispatch(logout())}}>
                 <i className='fas fa-sign-out-alt'/>
                 <p>Log out</p>
             </li>
         </ul>
+    </div>:
+    <div className='mainDropDown' style={{height: 72, width: 125}}>
+        <ul>
+            <li style={{borderTopLeftRadius: 22, borderTopRightRadius: 22}}>
+                <i className='fas fa-plus'/>
+                <p>Sign Up</p>
+            </li>
+            <li>
+                <i className='fas fa-cog'/>
+                <p>Log In</p>
+            </li>
+            <li style={{borderBottomLeftRadius: 22, borderBottomRightRadius: 22}}>
+                <i className='fas fa-comment-dots'/>
+                <p>Demo</p>
+            </li>
+        </ul>
     </div>
+    }
 
 
     </>
