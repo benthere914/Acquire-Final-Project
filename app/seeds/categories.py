4732cfa5161170341cff428d4468e67570c729a8
Benjamin Rose <benthere914@gmail.com>
2021-11-16 13:16:33 -0600

    added category and item seeders

--- app/seeds/categories.py
+++ app/seeds/categories.py
@@ -3,12 +3,6 @@ from app.models import db, Category
 
 # Adds a demo user, you can add other users here if you want
 def seed_categories():
-    # demo = User(
-    #     username='Demo', email='demo@aa.io', password='password')
-    # marnie = User(
-    #     username='marnie', email='marnie@aa.io', password='password')
-    # bobbie = User(
-    #     username='bobbie', email='bobbie@aa.io', password='password')
 
     # db.session.add(demo)
     # db.session.add(marnie)
