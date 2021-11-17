10616f321ccc9b7a3ead6626ba17ddcb479f486f
Benjamin Rose <benthere914@gmail.com>
2021-11-16 22:18:01 -0600

    updated styling for search bar

--- react-app/src/components/search/index.js
+++ react-app/src/components/search/index.js
@@ -1,13 +1,49 @@
 import Dropdown from 'react-dropdown';
+
 import 'react-dropdown/style.css';
 import './index.css'
 const Search = () => {
-    const options = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 6, 4, 3, 2, 2, 43, 454, 8]
-    return (
+    const options = [
+        'All Categories',
+        'Antiques',
+        'Art',
+        'Baby',
+        'Books',
+        'Business & Industrial',
+        'Cameras & Photo',
+        'Clothing, Shoes, & Accessories',
+        'Coins & Paper Money',
+        'Collectibles',
+        'Computers/Tablets & Networking',
+        'Consumer Electronics',
+        'Crafts',
+        'Dolls & Bears',
+        'DVDs & Movies',
+        'Entertainment Memorabillia',
+        'Gift Cards & Coupons',
+        'Health & Beauty',
+        'Home & Garden',
+        'Jewely & Watches',
+        'Music',
+        'Musical Instruments & Gear',
+        'Pet Supplies',
+        'Pottery & Glass',
+        'Sporting Goods',
+        'Sports Memorabillia',
+        'Stamps',
+        'Tickets & Experiences',
+        'Toys & hobbies',
+        'Travel',
+        'Video Games & Consoles',
+        'Everything Else'
+    ]
+      return (
     <>
         <div className='search'>
-            <p>Shop by category</p>
-            <i className='fas fa-arrow-down'></i>
+            <div className='categoryModalButton'>
+                <p>Shop by category</p>
+                <i className='fas fa-arrow-down'></i>
+            </div>
             <input type='text' placeholder={'search for anything'}></input>
             <Dropdown options={options} placeholder='select an option'/>
         </div>
