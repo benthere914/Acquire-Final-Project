5bc2eb18678686f4413877b9f044f490515627ee
Benjamin Rose <benthere914@gmail.com>
2021-11-16 21:51:20 -0600

    created basic structure and styling for search bar

--- react-app/src/components/search/index.js
+++ react-app/src/components/search/index.js
@@ -1,7 +1,16 @@
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+import './index.css'
 const Search = () => {
+    const options = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 6, 4, 3, 2, 2, 43, 454, 8]
     return (
     <>
-        <p>test</p>
+        <div className='search'>
+            <p>Shop by category</p>
+            <i className='fas fa-arrow-down'></i>
+            <input type='text' placeholder={'search for anything'}></input>
+            <Dropdown options={options} placeholder='select an option'/>
+        </div>
     </>
     )
 }
