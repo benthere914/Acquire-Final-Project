3d32c72308e312e471547a81e06fa8a5f25168e0
Benjamin Rose <benthere914@gmail.com>
2021-11-16 23:03:10 -0600

    set auto play to the carousel

--- react-app/src/components/splashPage/index.js
+++ react-app/src/components/splashPage/index.js
@@ -7,7 +7,7 @@ const SplashPage = () => {
     return (
     <>
         <Search/>
-        <Carousel autoPlay={false} interval={3000} infiniteLoop={true}>
+        <Carousel autoPlay={true} interval={3000} infiniteLoop={true}>
             <CarouselCard id={1}/>
             <CarouselCard id={2}/>
             <CarouselCard id={3}/>
