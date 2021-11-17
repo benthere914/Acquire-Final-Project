8bcc4fda9ee6986360ee3e9529ee170d5d38000a
Benjamin Rose <benthere914@gmail.com>
2021-11-16 18:24:58 -0600

    added demo button and cleaned up a little code

--- react-app/src/components/footer/index.js
+++ react-app/src/components/footer/index.js
@@ -8,13 +8,14 @@ const Footer = () => {
             <img
                 className='footerLinkedInLink footerLink'
                 src='https://cdn3.iconfinder.com/data/icons/free-social-icons/67/linkedin_circle_black-512.png'
+                alt='LinkedIn'
                 onClick={() => {openSiteHandler('https://www.linkedin.com/in/benjamin-rose-0a6880202/')}}/>
 
             <img
                 className='footerGitHubLink footerLink'
                 src='https://cdn-icons-png.flaticon.com/512/25/25231.png'
+                alt='GitHub'
                 onClick={() => {openSiteHandler('https://github.com/benthere914/')}}/>
-
     </div>
     </>
     )
