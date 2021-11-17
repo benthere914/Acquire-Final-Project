bcbb0f60aac6ef6197b663b1263764b49d759a1e
Benjamin Rose <benthere914@gmail.com>
2021-11-16 20:58:03 -0600

    carousel working

--- react-app/src/components/splashPage/index.js
+++ react-app/src/components/splashPage/index.js
@@ -5,7 +5,7 @@ import { Carousel } from 'react-responsive-carousel';
 const SplashPage = () => {
     return (
     <>
-        <Carousel>
+        <Carousel autoPlay={true} interval={3000} infiniteLoop={true}>
             <CarouselCard id={1}/>
             <CarouselCard id={2}/>
             <CarouselCard id={3}/>
