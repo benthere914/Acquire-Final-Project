c2f596bc9da1b94b4592929073eadedca077a84b
Benjamin Rose <benthere914@gmail.com>
2021-11-18 19:36:31 -0600

    installed new file and messageBoard tabs have all the structure pieces

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, request
 from flask_login import login_required
 from app.models import User, Item, ItemPhoto, db, MessageBoard, Message
 import re
+from sqlalchemy import or_
 
 user_routes = Blueprint('users', __name__)
 
@@ -112,7 +113,8 @@ def delete_user(userId):
 def get_buyer_message_boards(userId):
     boards = {board.to_dict()['id']:board.to_dict() for board in MessageBoard.query.filter(MessageBoard.potentialBuyerId == userId).all()}
     for board in boards:
-        boards[board]['last message'] = Message.query.filter(Message.authorId == userId).filter(Message.messageBoardId == board).order_by(Message.createdAt.desc()).first().to_dict()['message'][0:40]
+        boards[board]['last_message'] = Message.query.filter(or_(Message.authorId == boards[board]['sellerId'], Message.authorId == boards[board]['potentialBuyerId'])).filter(Message.messageBoardId == board).order_by(Message.createdAt.desc()).first().to_dict()
+        boards[board]['user'] = User.query.filter(User.id != userId).filter(or_(User.id == boards[board]['sellerId'], User.id == boards[board]['potentialBuyerId'])).first().to_dict()
     return jsonify(boards)
 
 @user_routes.route('/<int:userId>/sellerMessageBoards')
@@ -120,5 +122,6 @@ def get_buyer_message_boards(userId):
 def get_seller_message_boards(userId):
     boards = {board.to_dict()['id']:board.to_dict() for board in MessageBoard.query.filter(MessageBoard.sellerId == userId).all()}
     for board in boards:
-        boards[board]['last message'] = Message.query.filter(Message.authorId == userId).filter(Message.messageBoardId == board).order_by(Message.createdAt.desc()).first().to_dict()['message'][0:40]
+        boards[board]['last_message'] = Message.query.filter(or_(Message.authorId == boards[board]['sellerId'], Message.authorId == boards[board]['potentialBuyerId'])).filter(Message.messageBoardId == board).order_by(Message.createdAt.desc()).first().to_dict()
+        boards[board]['user'] = User.query.filter(User.id != userId).filter(or_(User.id == boards[board]['sellerId'], User.id == boards[board]['potentialBuyerId'])).first().to_dict()
     return jsonify(boards)
--- react-app/src/components/messagesPage/index.js
+++ react-app/src/components/messagesPage/index.js
@@ -1,17 +1,31 @@
 import './index.css'
 import { useSelector, useDispatch } from 'react-redux'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { getBuyerMessageBoards } from '../../store/buyerMessageBoards'
 import { getSellerMessageBoards } from '../../store/sellerMessageBoards'
+const dateDiff = require('date-difference')
 const MessagesPage = () => {
+    const date = new Date()
+    console.log(date)
+    // console.log(dateDiff())
+    const [selectedBoard, setSelectedBoard] = useState('seller')
     const dispatch = useDispatch()
     const buyerMessageBoards = useSelector(state => Object.values(state.buyerMessageBoards))
     const sellerMessageBoards = useSelector(state => Object.values(state.sellerMessageBoards))
+    const selectedMessageBoards = useSelector(state => Object.values(state[`${selectedBoard}MessageBoards`]))
     const userId = useSelector(state => state.session.user.id)
     useEffect(() => {
         dispatch(getBuyerMessageBoards(userId))
         dispatch(getSellerMessageBoards(userId))
     }, [userId])
+    const dateConverter = (string) => {
+        const today = new Date()
+        const messageDate = new Date(string)
+        const diff = dateDiff(messageDate, today)
+        return diff.slice(0, 3)
+    }
+
+
     return (
     <>
         <div>
@@ -19,7 +33,14 @@ const MessagesPage = () => {
                 <p>Chats</p>
                 <input type='text' placeholder='Search Your Messages'></input>
                 <div>
-
+                    {selectedMessageBoards.map((board) => (
+                        <div key={board.id}>
+                            <img src={board?.user?.icon}/>
+                            <p>{board?.user?.username}</p>
+                            <p>{board?.last_message?.message}</p>
+                            <p>{dateConverter(board?.last_message?.createdAt)}</p>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
