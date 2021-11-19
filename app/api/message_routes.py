2e444f608e49d7e98017742a07a82fc4be4b89b6
Benjamin Rose <benthere914@gmail.com>
2021-11-18 22:11:33 -0600

    post crud for messageBoard

--- app/api/message_routes.py
+++ app/api/message_routes.py
@@ -1,8 +1,8 @@
 import re
 from flask import Blueprint, jsonify, request
 from flask_login import login_required
-from app.models import User, Message, MessageBoard
-from datetime import date
+from app.models import User, Message, MessageBoard, db
+from datetime import datetime
 
 message_routes = Blueprint('message', __name__)
 
@@ -10,5 +10,15 @@ message_routes = Blueprint('message', __name__)
 @message_routes.route('/', methods=['POST'])
 def send_message():
     body = request.get_json()
-    print(body)
-    return {'message': 'got here'}
+    message_boards = MessageBoard.query.filter(MessageBoard.sellerId == body['sellerId']).filter(MessageBoard.potentialBuyerId == body['buyerId']).all()
+    if len(message_boards) == 1:
+        db.session.add(Message(messageBoardId=message_boards[0].to_dict()['id'], authorId=body['sellerId'], message=body['message'], createdAt=datetime.now()))
+        db.session.commit()
+        return {'messageBoardId': message_boards[0].to_dict()['id']}
+    if len(message_boards) == 0:
+        message_board = MessageBoard(sellerId=body['sellerId'], potentialBuyerId=body['buyerId'])
+        db.session.add(message_board)
+        db.session.commit()
+        db.session.add(Message(messageBoardId=message_board.id, authorId=body['sellerId'], message=body['message'], createdAt=datetime.now()))
+        db.session.commit()
+        return {'messageBoardId': message_board.id}
