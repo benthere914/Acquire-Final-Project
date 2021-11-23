983b1df509f86eb3d393afb2bc523cc23bbd90cc
Benjamin Rose <benthere914@gmail.com>
2021-11-23 12:28:33 -0600

    password on signup

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -31,13 +31,26 @@ def short_enough(form, field):
     if len(field.data) > 20:
         raise ValidationError(' - Must be no more than 20 characters')
 
-def valid_password(form, field):
+def password_long_enough(form, field):
     if len(field.data) < 8:
         raise ValidationError(' - Must be at least 8 characters')
+
+def password_short_enough(form, field):
+    if len(field.data) > 25:
+        raise ValidationError(' - Must be at most 25 characters')
+
+def password_contains_letter(form, field):
+    if not bool(re.search('[a-z,A-Z]', field.data)):
+        raise ValidationError(' - Must have at least 1 letter')
+
+def password_contains_number(form, field):
+    if not bool(re.search('[0-9]', field.data)):
+        raise ValidationError(' - Must have at least 1 number')
+
 
 class SignUpForm(FlaskForm):
     username = StringField(
         'username', validators=[DataRequired(message=' - This field is required.'), username_exists, long_enough, short_enough])
     email = StringField('email', validators=[DataRequired(message=' - This field is required.'), user_exists, Email(), long_enough])
     icon = StringField('icon')
-    password = StringField('password', validators=[DataRequired(message=' - This field is required.'), valid_password])
+    password = StringField('password', validators=[DataRequired(message=' - This field is required.'), password_long_enough, password_contains_letter, password_contains_number])
